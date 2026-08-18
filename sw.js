7ee18602f64aa0f74cc7b621c837a09b97a74a27
Neau7lll <johan.dreamstar@gmail.com>
2026-08-18 08:34:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,70 @@
-const CACHE = "integrity-field-v0.2";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+const CACHE_NAME = "integrity-field-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_FILES))
+
+  );
+
   self.skipWaiting();
+
 });
+
 
 self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+
+    caches.keys().then(keys =>
+
+      Promise.all(
+
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+
+      )
+
+    )
+
   );
+
   self.clients.claim();
+
 });
 
+
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match("./index.html")))
+
+    caches.match(event.request)
+      .then(cachedResponse => {
+
+        if(cachedResponse){
+          return cachedResponse;
+        }
+
+        return fetch(event.request)
+          .catch(() => {
+
+            return caches.match("./index.html");
+
+          });
+
+      })
+
   );
+
 });
